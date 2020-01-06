29d7894de2113f8f365cbc761712d725753abeb7
Vordty <tokomostwanted@gmail.com>
2020-01-06 20:49:55 +0400

    Implement basic authentication functionalities

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,68 @@
+import React, { createContext, useState } from "react";
+
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { SIGNUP, LOGIN } from "../queries/AuthQueries";
+import { AuthToken } from "../helpers/Auth";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	const [signupMutation] = useMutation(SIGNUP);
+	const [loginMutation] = useMutation(LOGIN);
+
+	const signup = async () => {
+		const {
+			data: { signup }
+		} = await signupMutation({
+			variables: {
+				firstName: "axalisaxeli",
+				lastName: "axaligvari",
+				email: "axaliemail2@gmail.com",
+				password: "axalipass123",
+				rePassword: "axalipass123"
+			}
+		});
+
+		console.log("Signup Data", signup);
+	};
+
+	const login = async () => {
+		const {
+			data: { login }
+		} = await loginMutation({
+			variables: {
+				email: "axaliemail2@gmail.com",
+				password: "axalipass123"
+			}
+		});
+
+		const token = AuthToken.GET();
+		if (isAuthenticated || token) {
+			console.log("Already logged in");
+			return;
+		}
+
+		if (login.success) {
+			console.log("Logging In...");
+			AuthToken.SET(login.token);
+			setIsAuthenticated(true);
+		}
+
+		console.log("Login Data", login);
+	};
+
+	const logout = () => {
+		const token = AuthToken.GET();
+		if (isAuthenticated || token) {
+			setIsAuthenticated(false);
+			AuthToken.REMOVE();
+			console.log("Logging Out...");
+		} else {
+			console.log("You are not logged in to logout");
+		}
+	};
+
+	return <AuthContext.Provider value={{ isAuthenticated, signup, login, logout }}>{children}</AuthContext.Provider>;
+};
