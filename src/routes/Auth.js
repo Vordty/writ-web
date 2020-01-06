bf4efb7f2553fd027abcdceda880e232efee1d41
Vordty <tokomostwanted@gmail.com>
2020-01-06 20:50:50 +0400

    Add basic ui setup for auth testing

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
 export const Auth = () => {
+	const { isAuthenticated, login, logout, signup } = useContext(AuthContext);
+
+	const onSignup = () => {
+		signup();
+	};
+
+	const onLogin = () => {
+		login();
+	};
+
+	const onLogout = () => {
+		logout();
+	};
+
 	return (
 		<div>
-			<h3>Auth Path (Login/Signup)</h3>
+			<h3>isAuthenticated: {isAuthenticated ? "True" : "False"}</h3>
+			<button onClick={onSignup}>Signup</button>
+			<button onClick={onLogin}>Login</button>
+			<button onClick={onLogout}>Logout</button>
 		</div>
 	);
 };
