7c29ac1a9f3efbc243e1b9d3c4f071faab77e71e
Vordty <tokomostwanted@gmail.com>
2020-01-06 20:49:36 +0400

    Add auth provider globally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Auth from "./routes/Auth";
 import Writ from "./routes/Writ";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
 	return (
-		<div className="App">
-			<Router>
-				<Switch>
-					{/* check if authenticated and decide route */}
-					<Route path="/auth">
-						<Auth />
-					</Route>
-					<Route path="/app">
-						<Writ />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
+		<AuthProvider>
+			<div className="App">
+				<Router>
+					<Switch>
+						{/* check if authenticated and decide route */}
+						<Route path="/auth">
+							<Auth />
+						</Route>
+						<Route path="/app">
+							<Writ />
+						</Route>
+					</Switch>
+				</Router>
+			</div>
+		</AuthProvider>
 	);
 }
 
