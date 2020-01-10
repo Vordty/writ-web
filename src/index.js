02181c4d667db340864b39981be5a8ba32d401fd
Vordty <tokomostwanted@gmail.com>
2020-01-10 15:16:31 +0400

    Add default headers for authorization

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { ApolloLink, concat } from "apollo-link";
 
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
@@ -14,9 +15,19 @@ const link = new HttpLink({
 	uri: "http://localhost:4000/graphql"
 });
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+	operation.setContext({
+		headers: {
+			authorization: localStorage.getItem("AuthToken") || null
+		}
+	});
+
+	return forward(operation);
+});
+
 const client = new ApolloClient({
 	cache,
-	link
+	link: concat(authMiddleware, link)
 });
 
 ReactDOM.render(
