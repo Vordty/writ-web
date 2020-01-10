670d73b75330632df8fe10b7a8dc42644ec28467
Vordty <tokomostwanted@gmail.com>
2020-01-10 15:17:56 +0400

    Use test query for fetching users

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { GET_USERS } from "../queries/UserQueries";
+
+import { useLazyQuery } from "@apollo/react-hooks";
 
 export const Auth = () => {
 	const { isAuthenticated, login, logout, signup } = useContext(AuthContext);
+
+	const [getUsersQuery, { loading, data, error }] = useLazyQuery(GET_USERS);
+
+	if (loading && !data) return "Loading...";
+	if (error) return `Error! ${error.message}`;
 
 	const onSignup = () => {
 		signup();
@@ -16,12 +24,23 @@ export const Auth = () => {
 		logout();
 	};
 
+	const onGetUsers = async () => {
+		getUsersQuery();
+		if (data) {
+			console.log("data", data);
+		}
+	};
+
 	return (
 		<div>
 			<h3>isAuthenticated: {isAuthenticated ? "True" : "False"}</h3>
 			<button onClick={onSignup}>Signup</button>
 			<button onClick={onLogin}>Login</button>
 			<button onClick={onLogout}>Logout</button>
+			<button onClick={onGetUsers}>Get Users</button>
+
+			<h4>USERS</h4>
+			<div>{data && data.users.map(user => <div>{user.firstName}</div>)}</div>
 		</div>
 	);
 };
