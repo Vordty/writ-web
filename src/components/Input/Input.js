6c0337aef9b0988c86017f5d7447555894ffef8b
Vordty <tokomostwanted@gmail.com>
2020-01-18 02:24:18 +0400

    Modify Input component & styling

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./Input.scss";
 
-export const Input = ({ style }) => {
+export const Input = ({ style = "", containerStyle = "", placeholder = "Missing placeholder", hidden }) => {
+	const inputType = hidden ? "password" : "text";
+
 	return (
-		<div className={"input-container" + " " + style}>
-			<input className="input" type="text" />
+		<div className={"input-container" + " " + containerStyle}>
+			<input className={"input" + " " + style} type={inputType} placeholder={placeholder} />
 		</div>
 	);
 };
