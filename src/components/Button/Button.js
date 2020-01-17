3f445139d161cac4b4a63b081a818711a93a9821
Vordty <tokomostwanted@gmail.com>
2020-01-18 02:23:56 +0400

    Add Button component & styling

---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Text from "../Text/Text";
+
+import "./Button.scss";
+
+export const Button = ({ type, isSmall, style = "", children }) => {
+	const textType = isSmall ? "small" : "regular";
+	return (
+		<div className="button-container">
+			<button className={type + " " + style}>
+				<Text type={textType}>{children}</Text>
+			</button>
+		</div>
+	);
+};
+
+export default Button;
