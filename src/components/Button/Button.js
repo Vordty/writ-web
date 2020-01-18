1c11bab7c344b3a5a7863171a97ba4567067ac33
Vordty <tokomostwanted@gmail.com>
2020-01-19 00:41:49 +0400

    Minimize text styling of Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,8 +4,7 @@ import Text from "../Text/Text";
 
 import "./Button.scss";
 
-const Button = ({ type, isSmall, style = "", children }) => {
-	const textType = isSmall ? "small" : "regular";
+const Button = ({ type, textType = "regular", style = "", children }) => {
 	return (
 		<div className="button-container">
 			<button className={type + " " + style}>
