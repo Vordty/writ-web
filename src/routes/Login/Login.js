b764089beb0276b7a1fd67cf3a26ec5e45fa9d9f
Vordty <tokomostwanted@gmail.com>
2020-01-24 16:55:05 +0400

    Add Login form states

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 
 import Background from "../../components/Background/Background";
 import Card from "../../components/Card/Card";
@@ -7,21 +8,53 @@ import Spacer from "../../components/Spacer/Spacer";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 
+import { AuthContext } from "../../contexts/AuthContext";
+
 const Login = () => {
+	const [form, setForm] = useState({
+		email: "",
+		password: ""
+	});
+
+	const { login } = useContext(AuthContext);
+
+	const onChange = e => {
+		setForm({
+			...form,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const onLogin = () => {
+		login(form);
+	};
+
 	return (
 		<Background pattern="dots">
 			<Button type="btn-ghost" style="capitalize corner-tr mt-4 mr-4">
-				Sign Up
+				<Link to="/signup">Sign Up</Link>
 			</Button>
 			<Card>
 				<div className="mtpc-5">
 					<Text type="title">LOGIN</Text>
 				</div>
 				<Spacer count="18" symbol="~" />
-				<Input containerStyle="mb" placeholder="Type your email" />
-				<Input hidden={true} placeholder="Type your password" />
+				<Input
+					containerStyle="mb"
+					placeholder="Type your email"
+					name="email"
+					value={form.email}
+					onChange={onChange}
+				/>
+				<Input
+					hidden={true}
+					placeholder="Type your password"
+					name="password"
+					value={form.password}
+					onChange={onChange}
+				/>
 				<Spacer count="18" symbol="~" />
-				<Button type="btn-solid" style="capitalize">
+				<Button type="btn-solid" style="capitalize" onClick={onLogin}>
 					Log In
 				</Button>
 				<div className="mbpc-5 mt-2">
