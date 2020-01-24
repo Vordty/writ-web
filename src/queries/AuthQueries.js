29765aa86f8227855eae0f7a563d87852dbaeb26
Vordty <tokomostwanted@gmail.com>
2020-01-24 16:54:06 +0400

    Modify signup query

--- src/queries/AuthQueries.js
+++ src/queries/AuthQueries.js
@@ -1,27 +1,13 @@
 import gql from "graphql-tag";
 
 export const SIGNUP = gql`
-	mutation Signup(
-		$firstName: String!
-		$lastName: String!
-		$email: String!
-		$password: String!
-		$rePassword: String!
-	) {
-		signup(
-			signupInput: {
-				firstName: $firstName
-				lastName: $lastName
-				email: $email
-				password: $password
-				rePassword: $rePassword
-			}
-		) {
+	mutation Signup($email: String!, $username: String!, $password: String!, $rePassword: String!) {
+		signup(signupInput: { email: $email, username: $username, password: $password, rePassword: $rePassword }) {
 			success
 			message
 			user {
 				id
-				firstName
+				username
 			}
 		}
 	}
