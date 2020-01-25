8afc36516b7efa8d8d3f9b786db0200f56744325
Vordty <tokomostwanted@gmail.com>
2020-01-25 20:13:29 +0400

    Modify text component

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./Text.scss";
 
-const Text = ({ type, children }) => {
-	return <p className={type}>{children}</p>;
+const Text = ({ type, customStyle, children }) => {
+	return <p className={type + " " + customStyle}>{children}</p>;
 };
 
 export default Text;
