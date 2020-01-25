5bb51ee65214634f83c6ca315d867608b826dc57
Vordty <tokomostwanted@gmail.com>
2020-01-25 20:12:05 +0400

    Modify button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,11 @@ import Text from "../Text/Text";
 
 import "./Button.scss";
 
-const Button = ({ type, textType = "regular", style = "", children, ...props }) => {
+const Button = ({ type, style = "", children, ...props }) => {
 	return (
 		<div className="button-container">
 			<button {...props} className={type + " " + style}>
-				<Text type={textType}>{children}</Text>
+				{children}
 			</button>
 		</div>
 	);
