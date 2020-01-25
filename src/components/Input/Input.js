ed68a79d6a02343a0410f40dc2d98561dbed1991
Vordty <tokomostwanted@gmail.com>
2020-01-25 20:12:47 +0400

    Add verification type for input

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,12 +2,19 @@ import React from "react";
 
 import "./Input.scss";
 
-const Input = ({ style = "", containerStyle = "", placeholder = "Missing placeholder", hidden, ...props }) => {
+const Input = ({ style = "", containerStyle = "", placeholder = "", hidden, maxLength = "48", inputRef, ...props }) => {
 	const inputType = hidden ? "password" : "text";
 
 	return (
 		<div className={"input-container" + " " + containerStyle}>
-			<input {...props} className={"input" + " " + style} type={inputType} placeholder={placeholder} />
+			<input
+				{...props}
+				ref={inputRef}
+				className={"input" + " " + style}
+				type={inputType}
+				placeholder={placeholder}
+				maxLength={maxLength}
+			/>
 		</div>
 	);
 };
