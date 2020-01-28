7df4e90ea2007e32e41ff39352717688278b35a1
Vordty <tokomostwanted@gmail.com>
2020-01-28 23:17:42 +0400

    Start building Editor component

---
+++ src/routes/Editor/Editor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Sidebar from "../../components/Shared/Sidebar/Sidebar";
+
+const Editor = () => {
+	return (
+		<div className="editor">
+			<Sidebar />
+		</div>
+	);
+};
+
+export default Editor;
