d27efc7a581001edd29724b4cc3a1a3b185c670c
Vordty <tokomostwanted@gmail.com>
2020-02-07 19:18:29 +0400

    Add Draggable List component to editor

--- src/routes/Editor/Editor.js
+++ src/routes/Editor/Editor.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Sidebar from "../../components/Shared/Sidebar/Sidebar";
+import ListDraggable from "../../components/List/ListDraggable/ListDraggable";
+
+import "./Editor.scss";
 
 const Editor = () => {
 	return (
 		<div className="editor">
 			<Sidebar />
+			<ListDraggable />
 		</div>
 	);
 };
