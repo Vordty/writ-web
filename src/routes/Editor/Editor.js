2209863ebabe04449a1cf9056e38e17fab0883cc
Vordty <tokomostwanted@gmail.com>
2020-02-10 17:49:46 +0400

    Add file explorer demo in Editor

--- src/routes/Editor/Editor.js
+++ src/routes/Editor/Editor.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Sidebar from "../../components/Shared/Sidebar/Sidebar";
+import Explorer from "../../components/Shared/Explorer/Explorer";
 import ListDraggable from "../../components/List/ListDraggable/ListDraggable";
 
 import "./Editor.scss";
@@ -8,14 +9,18 @@ import "./Editor.scss";
 const Editor = () => {
 	return (
 		<div className="editor">
-			<Sidebar />
+			<Sidebar header="Explorer">
+				<Explorer></Explorer>
+			</Sidebar>
 			<div className="w-100">
-				<ListDraggable customStyle="flex-h">
-					<div>TOOL 1</div>
-					<div>TOOL 2</div>
-					<div>TOOL 3</div>
-				</ListDraggable>
-				<div className="opened-files-container">
+				<div className="pl-4">
+					<ListDraggable customStyle="flex-h">
+						<div>TOOL 1</div>
+						<div>TOOL 2</div>
+						<div>TOOL 3</div>
+					</ListDraggable>
+				</div>
+				<div className="opened-files-container pl-4">
 					<ListDraggable customStyle="flex-h">
 						<div>FILE 1</div>
 						<div>FILE 2</div>
