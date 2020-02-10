b0875e16a4d4680c571a6ed84fbe09fdf3112e75
Vordty <tokomostwanted@gmail.com>
2020-02-10 17:49:27 +0400

    Modify sidebar for file explorer

--- src/components/Shared/Sidebar/Sidebar.js
+++ src/components/Shared/Sidebar/Sidebar.js
@@ -4,15 +4,18 @@ import Resizable from "../../Resizable/Resizable";
 
 import "./Sidebar.scss";
 
-const Sidebar = () => {
+const Sidebar = ({ header, children }) => {
 	return (
 		<div className="sidebar">
 			<div className="sidebar-options">
-				<div className="sidebar-option">ITEM 1</div>
-				<div className="sidebar-option">ITEM 1</div>
-				<div className="sidebar-option">ITEM 1</div>
+				<div className="sidebar-option">1</div>
+				<div className="sidebar-option">2</div>
+				<div className="sidebar-option">3</div>
 			</div>
-			<Resizable customStyle="sidebar-content" />
+			<Resizable customStyle="sidebar-content" minWidth={200} maxWidth={400}>
+				<div className="sidebar-header">{header}</div>
+				{children}
+			</Resizable>
 		</div>
 	);
 };
