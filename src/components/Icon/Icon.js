07ba81c7c97134d8ebf5af74ed94833ad070077b
Vordty <tokomostwanted@gmail.com>
2020-02-27 16:28:23 +0400

    Make Icon component more modular

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,9 +1,29 @@
 import React from "react";
 
+import { ReactComponent as Dot3SVG } from "../../assets/svgs/3dot.svg";
+import { ReactComponent as CheckmarkSVG } from "../../assets/svgs/checkmark.svg";
+import { ReactComponent as PlusSVG } from "../../assets/svgs/plus.svg";
+import { ReactComponent as XSVG } from "../../assets/svgs/x.svg";
+import { ReactComponent as Arrow } from "../../assets/svgs/arrow.svg";
+
 import "./Icon.scss";
 
-const Icon = ({ type }) => {
-	return <div className={type}></div>;
+const Icon = ({ type, style = "", onClick }) => {
+	const _type = onClick ? type : type + " cursor-pointer";
+	const props = { className: _type + ` ${style}`, onClick: () => console.log(type + " Clicked") };
+
+	switch (type) {
+		case "plus":
+			return <PlusSVG {...props} />;
+		case "x":
+			return <XSVG {...props} />;
+		case "checkmark":
+			return <CheckmarkSVG {...props} />;
+		case "dot3":
+			return <Dot3SVG {...props} />;
+		case "arrow":
+			return <Arrow {...props} />;
+	}
 };
 
 export default Icon;
