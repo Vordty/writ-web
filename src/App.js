5849d32c739362814f83c8f088799a152568faf9
Vordty <tokomostwanted@gmail.com>
2020-02-28 23:44:52 +0400

    Wrap app into Component context provider

--- src/App.js
+++ src/App.js
@@ -7,26 +7,29 @@ import Editor from "./routes/Editor/Editor";
 import Verification from "./routes/Verification/Verification";
 import Projects from "./routes/Projects/Projects";
 
-import PrivateRoute from "./routes/Auth/PrivateRoute";
-import AuthRoute from "./routes/Auth/AuthRoute";
+import PrivateRoute from "./routes/RouteTypes/PrivateRoute";
+import AuthRoute from "./routes/RouteTypes/AuthRoute";
 
 import { AuthProvider } from "./contexts/AuthContext";
+import { ComponentProvider } from "./contexts/ComponentContext";
 
 function App() {
 	return (
 		<AuthProvider>
-			<div className="App">
-				<Router>
-					<Switch>
-						<Redirect exact from="/" to="/app" />
-						<AuthRoute path="/login" component={Login} redirectPath="/app" />
-						<AuthRoute path="/signup" component={Signup} redirectPath="/app" />
-						<Route path="/verification" component={Verification} />
-						<PrivateRoute path="/projects" component={Projects} />
-						<PrivateRoute path="/app" component={Editor} />
-					</Switch>
-				</Router>
-			</div>
+			<ComponentProvider>
+				<div className="App">
+					<Router>
+						<Switch>
+							<Redirect exact from="/" to="/app" />
+							<AuthRoute path="/login" component={Login} redirectPath="/app" />
+							<AuthRoute path="/signup" component={Signup} redirectPath="/app" />
+							<Route path="/verification" component={Verification} />
+							<PrivateRoute path="/projects" component={Projects} />
+							<PrivateRoute path="/app" component={Editor} />
+						</Switch>
+					</Router>
+				</div>
+			</ComponentProvider>
 		</AuthProvider>
 	);
 }
