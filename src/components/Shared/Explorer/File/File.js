3b3425b500f9ab9969870eee03bb34817ede4fa9
Vordty <tokomostwanted@gmail.com>
2020-03-01 18:55:42 +0400

    Change logic of recieving file menu related clicks

--- src/components/Shared/Explorer/File/File.js
+++ src/components/Shared/Explorer/File/File.js
@@ -5,33 +5,42 @@ import { ComponentContext } from "../../../../contexts/ComponentContext";
 
 import "./File.scss";
 
-const File = ({ id, title, level, levelIndentStep, onClick, isOpen, isFolder, children }) => {
-	const { onRightClick } = useContext(ComponentContext);
+const File = ({
+  id,
+  title,
+  level,
+  levelIndentStep,
+  onClick,
+  isOpen,
+  isFolder,
+  children
+}) => {
+  const { onMenuClick } = useContext(ComponentContext);
 
-	return isFolder ? (
-		<Fragment>
-			<div className="folder">
-				<div
-					className="folder-title"
-					onClick={() => onClick(id)}
-					onMouseUp={e => onRightClick(e, "folder")}
-					style={{ paddingLeft: `${level * levelIndentStep}px` }}
-				>
-					<Icon type="arrow" style={isOpen ? "arrow-open" : "arrow"} />
-					{title}
-				</div>
-				{isOpen && <div className="folder-content">{children}</div>}
-			</div>
-		</Fragment>
-	) : (
-		<div
-			className="file"
-			style={{ paddingLeft: `${level * levelIndentStep}px` }}
-			onMouseUp={e => onRightClick(e, "file")}
-		>
-			{title}
-		</div>
-	);
+  return isFolder ? (
+    <Fragment>
+      <div className="folder">
+        <div
+          className="folder-title"
+          onClick={() => onClick(id)}
+          onContextMenu={e => onMenuClick(e, "folder", "right")}
+          style={{ paddingLeft: `${level * levelIndentStep}px` }}
+        >
+          <Icon type="arrow" style={isOpen ? "arrow-open" : "arrow"} />
+          {title}
+        </div>
+        {isOpen && <div className="folder-content">{children}</div>}
+      </div>
+    </Fragment>
+  ) : (
+    <div
+      className="file"
+      style={{ paddingLeft: `${level * levelIndentStep}px` }}
+      onContextMenu={e => onMenuClick(e, "file")}
+    >
+      {title}
+    </div>
+  );
 };
 
 export default File;
