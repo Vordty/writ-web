6d507dbcc7f30bc520e8434622612c102846c426
Vordty <tokomostwanted@gmail.com>
2020-03-02 19:29:26 +0400

    Add double click functionality in file explorer

--- src/components/Shared/Explorer/File/File.js
+++ src/components/Shared/Explorer/File/File.js
@@ -2,45 +2,39 @@ import React, { Fragment, useContext, useRef } from "react";
 
 import Icon from "../../../Icon/Icon";
 import { ComponentContext } from "../../../../contexts/ComponentContext";
+import { FileContext } from "../../../../contexts/FileContext";
 
 import "./File.scss";
 
-const File = ({
-  id,
-  title,
-  level,
-  levelIndentStep,
-  onClick,
-  isOpen,
-  isFolder,
-  children
-}) => {
-  const { onMenuClick } = useContext(ComponentContext);
+const File = ({ id, title, level, levelIndentStep, onClick, isOpen, isFolder, children }) => {
+	const { onMenuClick } = useContext(ComponentContext);
+	const { openFile } = useContext(FileContext);
 
-  return isFolder ? (
-    <Fragment>
-      <div className="folder">
-        <div
-          className="folder-title"
-          onClick={() => onClick(id)}
-          onContextMenu={e => onMenuClick(e, "folder", "right")}
-          style={{ paddingLeft: `${level * levelIndentStep}px` }}
-        >
-          <Icon type="arrow" style={isOpen ? "arrow-open" : "arrow"} />
-          {title}
-        </div>
-        {isOpen && <div className="folder-content">{children}</div>}
-      </div>
-    </Fragment>
-  ) : (
-    <div
-      className="file"
-      style={{ paddingLeft: `${level * levelIndentStep}px` }}
-      onContextMenu={e => onMenuClick(e, "file")}
-    >
-      {title}
-    </div>
-  );
+	return isFolder ? (
+		<Fragment>
+			<div className="folder">
+				<div
+					className="folder-title"
+					onClick={() => onClick(id)}
+					onContextMenu={e => onMenuClick(e, "folder", "right")}
+					style={{ paddingLeft: `${level * levelIndentStep}px` }}
+				>
+					<Icon type="arrow" style={isOpen ? "arrow-open" : "arrow"} />
+					{title}
+				</div>
+				{isOpen && <div className="folder-content">{children}</div>}
+			</div>
+		</Fragment>
+	) : (
+		<div
+			className="file"
+			style={{ paddingLeft: `${level * levelIndentStep}px` }}
+			onContextMenu={e => onMenuClick(e, "file")}
+			onDoubleClick={() => openFile(id)}
+		>
+			{title}
+		</div>
+	);
 };
 
 export default File;
