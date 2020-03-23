175c36249b1c48f415a0d35b8af60863a546a90a
Vordty <tokomostwanted@gmail.com>
2020-03-23 23:38:28 +0400

    Implement file renaming functionality

--- src/components/Shared/Explorer/File/File.js
+++ src/components/Shared/Explorer/File/File.js
@@ -9,48 +9,69 @@ import { useOutsideAlerter } from "helpers/OutsideClick";
 
 const File = ({ file, levelIndentStep, onClick, children, ...props }) => {
 	const fileRef = useRef(null);
-	const { id, title, level, isOpen, isFolder, isRenaming } = file;
+	const { id, title, level, isOpen, isFolder } = file;
+
+	const [renameText, setRenameText] = useState(title);
 
 	const { onMenuClick } = useContext(ComponentContext);
-	const { openFile, turnOffRenameState } = useContext(FileContext);
+	const { openFile, turnOffRenameState, changeFileTitle, renameStateInfo } = useContext(FileContext);
 
-	useOutsideAlerter(fileRef, () => onFileOutsideClick(), ["resizable-drag-line"]);
+	useOutsideAlerter(fileRef, () => onRenameOutsideClick(), ["resizable-drag-line"]);
 
-	const onFileOutsideClick = () => {
+	const isRenaming = renameStateInfo.isActive && renameStateInfo.fileId === id;
+
+	const onRenameOutsideClick = () => {
 		if (isRenaming) {
 			turnOffRenameState(id);
+			changeFileTitle(id, renameText);
 		}
 	};
 
-	let folderClassName = "folder";
-	let fileClassName = "file";
-
-	if (isRenaming) {
-		folderClassName += " folder-rename";
-		fileClassName += " file-rename";
-	}
+	const onRenameChange = e => {
+		setRenameText(e.target.value);
+	};
 
 	return isFolder ? (
 		<Fragment>
-			<div className={folderClassName} ref={fileRef}>
-				<div
-					{...props}
-					className="folder-title"
-					onClick={() => onClick(id)}
-					onContextMenu={e => onMenuClick(e, "folder", id)}
-					style={{ paddingLeft: `${level * levelIndentStep}px` }}
-				>
-					<Icon type="arrow" style={isOpen ? "arrow-open" : "arrow"} />
-					{title}
-				</div>
+			<div className="folder" ref={fileRef}>
+				{isRenaming ? (
+					<div
+						style={{
+							marginLeft: `${level * levelIndentStep}px`
+						}}
+						className="one-liner"
+					>
+						<Icon type="arrow" customStyle={isOpen ? "arrow-open" : "arrow"} />
+						<input value={renameText} className="folder-rename" onChange={onRenameChange} />
+					</div>
+				) : (
+					<div
+						{...props}
+						className="folder-title overflow-dots"
+						onClick={() => onClick(id)}
+						onContextMenu={e => onMenuClick(e, "folder", id)}
+						style={{ paddingLeft: `${level * levelIndentStep}px` }}
+					>
+						<Icon type="arrow" customStyle={isOpen ? "arrow-open" : "arrow"} />
+						{title}
+					</div>
+				)}
 				{isOpen && <div className="folder-content">{children}</div>}
 			</div>
 		</Fragment>
+	) : isRenaming ? (
+		<input
+			ref={fileRef}
+			value={renameText}
+			onChange={onRenameChange}
+			className="folder-rename"
+			style={{ marginLeft: `${level * levelIndentStep}px` }}
+		/>
 	) : (
 		<div
 			{...props}
 			ref={fileRef}
-			className={fileClassName}
+			className="file overflow-dots"
 			style={{ paddingLeft: `${level * levelIndentStep}px` }}
 			onContextMenu={e => onMenuClick(e, "file", id)}
 			onDoubleClick={() => openFile(id)}
