afd1e8e6d8b9967e5d1b6ac721ef4ec8ec7783b0
Vordty <tokomostwanted@gmail.com>
2020-05-25 18:06:04 +0400

    Add Leaf types

--- src/panels/TextEditor/TextEditor.js
+++ src/panels/TextEditor/TextEditor.js
@@ -1,12 +1,82 @@
-import React, { useContext } from "react";
+// Import React dependencies.
+import React, { useEffect, useMemo, useState, useRef } from "react";
 
-import { FileContext } from "../../contexts/FileContext";
+import { createEditor, Editor, Transforms, Text, Range } from "slate";
+import { Slate, Editable, withReact, useSlate, ReactEditor } from "slate-react";
+
+import Toolbar from "./Toolbar/Toolbar";
 
 import "./TextEditor.scss";
 
 const TextEditor = () => {
-	const { displayedFile } = useContext(FileContext);
-	return <div className="text-editor">{displayedFile.title}</div>;
+	const editor = useMemo(() => withReact(createEditor()), []);
+
+	const [value, setValue] = useState([
+		{
+			type: "paragraph",
+			children: [{ text: "A line of text in a paragraph." }],
+		},
+	]);
+
+	useEffect(() => {
+		console.log("TEXT EDITOR CHANGED");
+	}, [value]);
+
+	const toggleFormat = (editor, format) => {
+		const isActive = isFormatActive(editor, format);
+		Transforms.setNodes(editor, { [format]: isActive ? null : true }, { match: Text.isText, split: true });
+	};
+
+	const isFormatActive = (editor, format) => {
+		const [match] = Editor.nodes(editor, {
+			match: n => n[format] === true,
+			mode: "all",
+		});
+		return !!match;
+	};
+
+	const Leaf = ({ attributes, children, leaf }) => {
+		if (leaf.bold) {
+			children = <strong>{children}</strong>;
+		}
+
+		if (leaf.italic) {
+			children = <em>{children}</em>;
+		}
+
+		if (leaf.underlined) {
+			children = <u>{children}</u>;
+		}
+
+		return <span {...attributes}>{children}</span>;
+	};
+
+	return (
+		<div className="text-editor-container">
+			<div className="text-editor">
+				<Slate editor={editor} value={value} onChange={value => setValue(value)}>
+					<Toolbar isFormatActive={isFormatActive} toggleFormat={toggleFormat} />
+					<Editable
+						renderLeaf={props => <Leaf {...props} />}
+						onDOMBeforeInput={e => {
+							switch (e.inputType) {
+								case "formatBold":
+									e.preventDefault();
+									return toggleFormat(editor, "bold");
+								case "formatItalic":
+									e.preventDefault();
+									return toggleFormat(editor, "italic");
+								case "formatUnderline":
+									e.preventDefault();
+									return toggleFormat(editor, "underline");
+								default:
+							}
+						}}
+					/>
+				</Slate>
+			</div>
+		</div>
+	);
 };
 
 export default TextEditor;
